f7da024090d17654b7bf925d6382f7f00ca5a498
kampsjaak <sjors.janssen@rotorgames.nl>
2021-04-14 20:23:24 +0200

    Everything is a member of pooptorio!

--- control.lua
+++ control.lua
@@ -1,44 +1,48 @@
-character = nil;
-need = 0;
-time = 0;
-listDump = {}
+Pooptorio = {}
+Pooptorio.character = nil
+Pooptorio.need = 0
+Pooptorio.time = 0
+Pooptorio.listDump = {}
 
-function on_console_chat()
-	if character then
-		character.health = character.health * ( 1 / character.get_health_ratio()) / character.get_health_ratio()
-		table.insert(listDump, character.surface.create_entity{
+function Pooptorio.on_console_chat()
+	if Pooptorio.character then
+		Pooptorio.character.health = Pooptorio.character.health * ( 1 / Pooptorio.character.get_health_ratio()) / Pooptorio.character.get_health_ratio()
+		table.insert(Pooptorio.listDump, Pooptorio.character.surface.create_entity{
 			name = "dead-dry-hairy-tree",
-			position = character.position,
+			position = Pooptorio.character.position,
 		})
-		character.surface.create_entity{
+		Pooptorio.character.surface.create_entity{
 			name = "acid-splash-fire-spitter-medium",
-			position = character.position,
+			position = Pooptorio.character.position,
 		}
 	end
 end
 
-function on_tick()
-	if not character then
+function Pooptorio.on_tick()
+	if not Pooptorio.poopBar then
+		Pooptorio.poopBar = game.get_player(1).gui.top.add{ type="progressbar", name="greeg2", value=0.5 }
+	end
+	if not Pooptorio.character then
 		if game.get_player(1) and game.get_player(1).character then
-			character = game.get_player(1).character
+			Pooptorio.character = game.get_player(1).character
 		else
 			game.print("no character")
 			return
 		end
 	else
-		if not character then
+		if not Pooptorio.character then
 			return
 		end
-		character.health = character.health - 10.0  
+		Pooptorio.character.health = Pooptorio.character.health - 10.0  
 		-- time = time + 1
 	end
 
-	for k,v in ipairs(listDump) do
+	for k,v in ipairs(Pooptorio.listDump) do
 		v.health = v.health - 3 
 	end
 end
 
-function canThePopeShitInTheWood(player)
+function Pooptorio.canThePopeShitInTheWood(player)
 	local count = 0
 	for _, entity in ipairs(player.surface.find_entities_filtered
 		{ 
@@ -51,18 +55,18 @@ function canThePopeShitInTheWood(player)
 	return count > 3
 end
 
-function on_player_used_capsule(e)
+function Pooptorio.on_player_used_capsule(e)
 	if e and e.item then
 		if e.item.name == "raw-fish" then 
 			game.print(string.format("Player %s ate %s", e.player_index, e.item.name))
-			need = need + 1
+			Pooptorio.need = Pooptorio.need + 1
 		end
 	end
 	--game.print("on_player_used_capsule")
 end
-function on_player_respawned()
+function Pooptorio.on_player_respawned()
 	game.print("on_player_respawned")
-	character = nil
+	Pooptorio.character = nil
 end
 
 script.on_event(defines.events.on_player_changed_position,
@@ -72,7 +76,7 @@ script.on_event(defines.events.on_player_changed_position,
       -- create the fire where they're standing
 
 
-      if canThePopeShitInTheWood(player) then
+      if Pooptorio.canThePopeShitInTheWood(player) then
       	player.surface.create_entity{name="item-on-ground", position=player.position, stack={name="turd"}}
 
       	local poopcount = 1;
@@ -80,9 +84,9 @@ script.on_event(defines.events.on_player_changed_position,
   end
 )
 
-script.on_event(defines.events.on_console_chat, on_console_chat, nil)
-script.on_event(defines.events.on_player_used_capsule, function(event) on_player_used_capsule(event) end, nil)
-script.on_event(defines.events.on_player_respawned, on_player_respawned, nil)
-script.on_nth_tick(600, on_tick)
+script.on_event(defines.events.on_console_chat, Pooptorio.on_console_chat, nil)
+script.on_event(defines.events.on_player_used_capsule, function(event) Pooptorio.on_player_used_capsule(event) end, nil)
+script.on_event(defines.events.on_player_respawned, Pooptorio.on_player_respawned, nil)
+script.on_nth_tick(600, Pooptorio.on_tick)
 
     --- game.player.gui.top.add{type="label", name="greeting2", caption="meowmeow"}
