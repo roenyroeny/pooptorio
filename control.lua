e8c2adcf3c4c3cecda18ba43cd0f89a84e49662c
kampsjaak <sjors.janssen@rotorgames.nl>
2021-04-14 21:44:51 +0200

    asdf
    asdf

--- control.lua
+++ control.lua
@@ -1,28 +1,30 @@
 Pooptorio = {}
-Pooptorio.character = nil
 Pooptorio.need = 0
 Pooptorio.tick = 0
 Pooptorio.listDump = {}
 
-function Pooptorio.on_console_chat()
-	if Pooptorio.character then
-		Pooptorio.character.health = Pooptorio.character.health * ( 1 / Pooptorio.character.get_health_ratio()) / Pooptorio.character.get_health_ratio()
-		table.insert(Pooptorio.listDump, Pooptorio.character.surface.create_entity{
-			name = "dead-dry-hairy-tree",
-			position = Pooptorio.character.position,
-		})
-		Pooptorio.character.surface.create_entity{
-			name = "acid-splash-fire-spitter-medium",
-			position = Pooptorio.character.position,
-		}
-	end
+function Pooptorio.on_console_chat(e)
+	local character = game.get_player(e.player_index).character
+	if not character then return end
+
+	character.health = character.health * ( 1 / character.get_health_ratio()) / character.get_health_ratio()
+	table.insert(Pooptorio.listDump, character.surface.create_entity{
+		name = "dead-dry-hairy-tree",
+		position = character.position,
+	})
+	character.surface.create_entity{
+		name = "acid-splash-fire-spitter-medium",
+		position = character.position,
+	}
 end
 
-function Pooptorio.on_tick()
-	Pooptorio.character.health = Pooptorio.character.health - 10.0
+function Pooptorio.on_tick(player)
+	if player.character then
+		player.character.health = player.character.health - 10.0
+	end
 
 	for k,v in ipairs(Pooptorio.listDump) do
-		v.health = v.health - 3 
+		v.health = v.health - 3
 	end
 end
 
@@ -32,7 +34,7 @@ function Pooptorio.canThePopeShitInTheWood(player)
 		{ 
 			area = {{player.position.x-3, player.position.y-3}, {player.position.x+3, player.position.y+3}
 		},
-		 type="tree"}) 
+		type="tree"}) 
 	do
 		count = count + 1
 	end
@@ -47,9 +49,9 @@ function Pooptorio.on_player_used_capsule(e)
 		end
 	end
 end
+
 function Pooptorio.on_player_respawned()
 	game.print("on_player_respawned")
-	Pooptorio.character = nil
 end
 
 script.on_event(defines.events.on_player_changed_position,
@@ -58,7 +60,6 @@ script.on_event(defines.events.on_player_changed_position,
     -- if they're wearing our armor
       -- create the fire where they're standing
 
-
       if Pooptorio.canThePopeShitInTheWood(player) then
       	player.surface.create_entity{name="item-on-ground", position=player.position, stack={name="turd"}}
       	local poopcount = 1;
@@ -66,33 +67,16 @@ script.on_event(defines.events.on_player_changed_position,
   end
 )
 
-function GetCharacter()
-	if Pooptorio.character then
-		return true
-	else if not Pooptorio.character then
-		if game.get_player(1) and game.get_player(1).character then
-			Pooptorio.character = game.get_player(1).character
-			return true
-		else
-			game.print("no character")
-		end
-	end
-	return false
-end
-
 function Pooptorio.main()
-	if Pooptorio.GetCharacter() then
-		if tick % 6 = 0 then
-			Pooptorio.on_tick()
+	for k,v in pairs(game.connected_players) do
+		if Pooptorio.tick % 6 == 0 then
+			Pooptorio.on_tick(v)
 		end
 	end
-	tick = tick + 1
+	Pooptorio.tick = Pooptorio.tick + 1
 end
 
-script.on_event(defines.events.on_console_chat, Pooptorio.on_console_chat, nil)
+script.on_event(defines.events.on_console_chat, function(event) Pooptorio.on_console_chat(event) end, nil)
 script.on_event(defines.events.on_player_used_capsule, function(event) Pooptorio.on_player_used_capsule(event) end, nil)
 script.on_event(defines.events.on_player_respawned, Pooptorio.on_player_respawned, nil)
-script.on_nth_tick(600, Pooptorio.on_tick)
 script.on_nth_tick(100, Pooptorio.main)
-
-    --- game.player.gui.top.add{type="label", name="greeting2", caption="meowmeow"}
